d583723d957392627cd976f5314ec5363691a197
zcittadin <z_cittadin@hotmail.com>
2017-07-19 10:00:38 -0300

    Criação de estrutura de aplicação

---
+++ src/main/java/com/servicos/estatica/resicolor/lab/app/App.java
@@ -0,0 +1,25 @@
+package com.servicos.estatica.resicolor.lab.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+		stage.setScene(new Scene(root));
+		stage.setTitle("Resicolor - Divisão de resinas");
+		// stage.setMaximized(true);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
---
+++ src/main/java/com/servicos/estatica/resicolor/lab/app/ControlledScreen.java
@@ -0,0 +1,9 @@
+package com.servicos.estatica.resicolor.lab.app;
+
+import com.servicos.estatica.resicolor.lab.controller.ScreensController;
+
+public interface ControlledScreen {
+
+	public void setScreenParent(ScreensController screenPage);
+
+}
