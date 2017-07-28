fb64c6c8c9cd357855843daf34662e2d221e5c7d
zcittadin <z_cittadin@hotmail.com>
2017-07-28 14:27:20 -0300

    Controle de animações

--- src/main/java/com/servicos/estatica/resicolor/lab/controller/InicialController.java
+++ src/main/java/com/servicos/estatica/resicolor/lab/controller/InicialController.java
@@ -62,13 +62,22 @@ public class InicialController implements Initializable, ControlledScreen {
 	@FXML
 	private Line lnChapa3;
 
-	Glow glow = new Glow(0);
-	SepiaTone sepia = new SepiaTone(0);
+	private static Image gifGlassFile = new Image("/com/servicos/estatica/resicolor/lab/style/glass.gif");
+	private static Image imgGlassFile = new Image("/com/servicos/estatica/resicolor/lab/style/glass.png");
+
+	private static Glow glow1 = new Glow(0);
+	private static SepiaTone sepia1 = new SepiaTone(0);
+	private static Glow glow2 = new Glow(0);
+	private static SepiaTone sepia2 = new SepiaTone(0);
+	private static Glow glow3 = new Glow(0);
+	private static SepiaTone sepia3 = new SepiaTone(0);
 
 	private static StrokeTransition chapaTransition1;
 	private static StrokeTransition chapaTransition2;
 	private static StrokeTransition chapaTransition3;
-	private static Timeline tmlAcende;
+	private static Timeline tmlHeater1;
+	private static Timeline tmlHeater2;
+	private static Timeline tmlHeater3;
 
 	ScreensController myController;
 
@@ -104,17 +113,67 @@ public class InicialController implements Initializable, ControlledScreen {
 		String sp = FxDialogs.showTextInput("Set-point", "Balão 3", "Digite o set-point:", "");
 		if (sp != null) {
 			txtSp3.setText(sp);
-
 		}
+	}
+
+	@FXML
+	private void initProc1() {
+		imgGlass1.setImage(gifGlassFile);
+		imgMola1.setEffect(sepia1);
+		chapaTransition1.play();
+		tmlHeater1.play();
+	}
+
+	@FXML
+	private void initProc2() {
+		imgGlass2.setImage(gifGlassFile);
+		imgMola2.setEffect(sepia2);
+		chapaTransition2.play();
+		tmlHeater2.play();
+	}
+
+	@FXML
+	private void initProc3() {
+		imgGlass3.setImage(gifGlassFile);
+		imgMola3.setEffect(sepia3);
+		tmlHeater3.play();
+		chapaTransition3.play();
+	}
+
+	@FXML
+	private void stopProc1() {
+		imgGlass1.setImage(imgGlassFile);
+		chapaTransition1.stop();
+		tmlHeater1.stop();
+		imgMola1.setEffect(null);
+		lnChapa1.setStroke(Color.RED);
+	}
+
+	@FXML
+	private void stopProc2() {
+		imgGlass2.setImage(imgGlassFile);
+		tmlHeater2.stop();
+		chapaTransition2.stop();
+		imgMola2.setEffect(null);
+		lnChapa2.setStroke(Color.RED);
+	}
+
+	@FXML
+	private void stopProc3() {
+		imgGlass3.setImage(imgGlassFile);
+		tmlHeater3.stop();
+		chapaTransition3.stop();
+		imgMola3.setEffect(null);
+		lnChapa3.setStroke(Color.RED);
 	}
 
 	private void configLayout() {
 		rect1.setFill(Color.TRANSPARENT);
 		rect2.setFill(Color.TRANSPARENT);
 		rect3.setFill(Color.TRANSPARENT);
-		imgGlass1.setImage(new Image("/com/servicos/estatica/resicolor/lab/style/glass.gif"));
-		imgGlass2.setImage(new Image("/com/servicos/estatica/resicolor/lab/style/glass.gif"));
-		imgGlass3.setImage(new Image("/com/servicos/estatica/resicolor/lab/style/glass.gif"));
+		imgGlass1.setImage(imgGlassFile);
+		imgGlass2.setImage(imgGlassFile);
+		imgGlass3.setImage(imgGlassFile);
 		JFXDepthManager.setDepth(imgGlass1, 5);
 		JFXDepthManager.setDepth(imgGlass2, 5);
 		JFXDepthManager.setDepth(imgGlass3, 5);
@@ -124,22 +183,39 @@ public class InicialController implements Initializable, ControlledScreen {
 		JFXDepthManager.setDepth(rect1, 5);
 		JFXDepthManager.setDepth(rect2, 5);
 		JFXDepthManager.setDepth(rect3, 5);
-		sepia.setInput(glow);
-		imgMola1.setEffect(sepia);
-		imgMola2.setEffect(sepia);
-		imgMola3.setEffect(sepia);
+		sepia1.setInput(glow1);
+		imgMola1.setEffect(sepia1);
+		sepia2.setInput(glow2);
+		imgMola2.setEffect(sepia2);
+		sepia3.setInput(glow3);
+		imgMola3.setEffect(sepia3);
 
 	}
 
 	private void configAnimations() {
-		tmlAcende = new Timeline(
-				new KeyFrame(Duration.ZERO, new KeyValue(glow.levelProperty(), 0.3),
-						new KeyValue(sepia.levelProperty(), 0.3)),
-				new KeyFrame(Duration.seconds(1), new KeyValue(glow.levelProperty(), 0.85),
-						new KeyValue(sepia.levelProperty(), 0.85)));
-		tmlAcende.setCycleCount(Timeline.INDEFINITE);
-		tmlAcende.setAutoReverse(true);
-		tmlAcende.play();
+		tmlHeater1 = new Timeline(
+				new KeyFrame(Duration.ZERO, new KeyValue(glow1.levelProperty(), 0.3),
+						new KeyValue(sepia1.levelProperty(), 0.3)),
+				new KeyFrame(Duration.seconds(1), new KeyValue(glow1.levelProperty(), 0.85),
+						new KeyValue(sepia1.levelProperty(), 0.85)));
+		tmlHeater1.setCycleCount(Timeline.INDEFINITE);
+		tmlHeater1.setAutoReverse(true);
+
+		tmlHeater2 = new Timeline(
+				new KeyFrame(Duration.ZERO, new KeyValue(glow2.levelProperty(), 0.3),
+						new KeyValue(sepia2.levelProperty(), 0.3)),
+				new KeyFrame(Duration.seconds(1), new KeyValue(glow2.levelProperty(), 0.85),
+						new KeyValue(sepia2.levelProperty(), 0.85)));
+		tmlHeater2.setCycleCount(Timeline.INDEFINITE);
+		tmlHeater2.setAutoReverse(true);
+
+		tmlHeater3 = new Timeline(
+				new KeyFrame(Duration.ZERO, new KeyValue(glow3.levelProperty(), 0.3),
+						new KeyValue(sepia3.levelProperty(), 0.3)),
+				new KeyFrame(Duration.seconds(1), new KeyValue(glow3.levelProperty(), 0.85),
+						new KeyValue(sepia3.levelProperty(), 0.85)));
+		tmlHeater3.setCycleCount(Timeline.INDEFINITE);
+		tmlHeater3.setAutoReverse(true);
 
 		chapaTransition1 = new StrokeTransition(Duration.millis(1000), lnChapa1, Color.RED, Color.ORANGE);
 		chapaTransition2 = new StrokeTransition(Duration.millis(1000), lnChapa2, Color.RED, Color.ORANGE);
@@ -150,9 +226,6 @@ public class InicialController implements Initializable, ControlledScreen {
 		chapaTransition1.setAutoReverse(true);
 		chapaTransition2.setAutoReverse(true);
 		chapaTransition3.setAutoReverse(true);
-		chapaTransition1.play();
-		chapaTransition2.play();
-		chapaTransition3.play();
 	}
 
 }
